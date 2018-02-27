13123807b22a17dd336748e19137c0ccab93fdbe
Heath <heathrost@gmail.com>
2018-02-26 17:09:21 -0700

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,111 @@
-$(document).ready(function() {
+window.onload = function() {
 
 //create array we'll use to start from and add more buttons to
-var topics = ['Pikachu', 'Mewtwo', "Mew", 'Articuno', 'Misty'];
+  // Initial array of pokemon
+  var pokemon = ['Pikachu', 'Mewtwo', "Mew", 'Articuno', 'Misty'];
 
-//creates our initial buttons by clearing them all then adding them back
-function createButtons(){
-	$('.buttons').empty();
-	for (var i= 0; i < topics.length; i++){
-		$('.buttons').css('color', 'blue').append("<button data-name='" + topics[i] + "'>" + topics[i] + "</button>");
-	}
+  function displayGif() {
+    $("#results").empty();
+
+    var pokemon = $(this).attr("data-name");
+
+    // Giphy API key
+    var APIKey = "ZnfZOhi0lEUfCLKmbun9KhR0Diw6Wx1x";
+
+    // QueryURL for Giphy API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + pokemon + "&limit=10&lang=en";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+
+      // Loop through results in object array
+      for (var i = 0; i < response.data.length; i++) {
+
+        // Create divs for each thumbnail and gif
+        var tvDiv = $("<div>");
+        tvDiv.addClass("col-md-4");
+        var thumbnailDiv = $("<div>");
+        thumbnailDiv.addClass("thumbnail");
+        tvDiv.append(thumbnailDiv);
+
+
+
+        // Create img tag and set state to still
+        var gif = $("<img class = 'gif'>");
+        gif.attr("data-state", "still");
+
+        // Store still image in variable and add to img tag
+        var stillImageUrl = response.data[i].images.fixed_width_still.url;
+        gif.attr("data-still", stillImageUrl);
+
+        // Store animated gif in variable with data-state
+        var animatedImageUrl = response.data[i].images.fixed_width.url;
+        gif.attr("data-animate", animatedImageUrl);
+
+        gif.attr("src", stillImageUrl);
+        thumbnailDiv.append(gif);
+
+        // Store rating in variable and add to p tag
+        var rating = response.data[i].rating;
+        var ratingUpperCase = rating.toUpperCase();
+        var pRating = $("<p>");
+        pRating.addClass("rating");
+        pRating.append("Rating: " + ratingUpperCase);
+        thumbnailDiv.append(pRating);
+
+        $("#results").append(tvDiv);
+
+      }
+    });
+  };
+
+  function displayButtons() {
+    $(".buttons").empty();
+
+    for (var i = 0; i < pokemon.length; i++) {
+      var button = $("<button>");
+      button.addClass("tvShow");
+      button.attr("data-name", pokemon[i]);
+      button.text(pokemon[i]);
+      $(".buttons").append(button);
+    }
+  }
+
+  function toggleImage() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+      console.log("Hello");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  }
+
+
+    $("#search-button").on("click", function(event) {
+      event.preventDefault();
+      // This line grabs the input from the textbox
+      var tvShow = $("#input").val().trim();
+
+      // Adding movie from the textbox to our array
+      pokemon.push(tvShow);
+
+      // Calling displayButtons which handles the processing of pokemon array
+      displayButtons();
+    });
+
+
+    // Adding a click event listener to all elements with a class of .tvShow, call displayGif function
+    $(document).on("click", ".tvShow", displayGif);
+
+    $(document).on("click", ".gif", toggleImage);
+
+
+    // Display buttons on page load
+    displayButtons();
+
 };
-createButtons();
-
-//creates a new button when add is clicked
-//adds the new button to the topics array
-$("#submitButton").on("click", function(event){
-		event.preventDefault();
-		var newButtonText= $('#searchInput').val();
-		topics.push(newButtonText);
-		createButtons()
-	})
-});
-
-//gather those gifs and display them
-//Display still images from giphy, with attributes referring to the animated images
-$("#buttons").on("click", "input", function() {
-	$("#buttons").empty();
-	var buttonText = $(this).attr("data-name");
-	queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=SKC76uSQiu2sf3ofblmPANRLXEr3QHJL&limit=10";
-
-	$.ajax({
-		url: queryURL,
-		method: 'GET'
-	}).done(function(response) {
-		var results = response.data;
-		console.log(results);
-		for (var i = 0; i < results.length; i++) {
-			$("#buttons").append('<img src="' + results[i].images.fixed_height_small_still.url + '" data-still="' + results[i].images.fixed_height_small_still.url + '" data-animate="' + results[i].images.fixed_height_small.url + '" data-state="still" class="img-responsive rounded gif">');
-		}
-	});
-});
-
-//onclick for our gifs, stops and starts them via their current state url, animation url and still url
-$(".myGifs").on("click", ".giphy", function(event) {
-	event.preventDefault();
-	var gifState = $(this).attr("data-state");
-	var animUrl = $(this).attr("data-anim");
-	var stillUrl = $(this).attr("data-still");
-	if (gifState == "still") {
-		$(this).attr("src", animUrl).attr('data-state', 'animate');
-	} else {
-		$(this).attr("src", stillUrl).attr('data-state', 'still');
-	}
-});
